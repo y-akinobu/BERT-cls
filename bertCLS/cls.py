d89cfb0e56e1798c9d24a8e8b5519221064b7bee
Yuka Akinobu <sx2_vn_yuka@outlook.jp>
2021-01-05 18:35:44 +0900

    updatedncls.py

--- bertCLS/cls.py
+++ bertCLS/cls.py
@@ -1,17 +1,14 @@
-# 必要なライブラリをインポート
 import numpy as np
 import transformers
 import torch
 from torch.utils.data import Dataset, DataLoader
-from transformers import BertJapaneseTokenizer,BertModel
+from transformers import BertJapaneseTokenizer, BertModel
 from torch import cuda
 import time
 from matplotlib import pyplot as plt
-# おまじない
 import logging
 logging.basicConfig(level=logging.ERROR)
 
-# Datasetの定義
 class CreateDataset(Dataset):
   def __init__(self, X, y, tokenizer, max_len):
     self.X = X
@@ -19,21 +16,20 @@ class CreateDataset(Dataset):
     self.tokenizer = tokenizer
     self.max_len = max_len
 
-
-  def __len__(self):  # len(Dataset)で返す値を指定
+  def __len__(self):
     return len(self.y)
 
-  def __getitem__(self, index):  # Dataset[index]で返す値を指定
-    text = self.X[index]
+  def __getitem__(self, index):
+    text = self.X(index)
     inputs = self.tokenizer.encode_plus(
       text,
-      add_special_tokens=True,
-      max_length=self.max_len,
-      padding='max_length',
-      truncation='longest_first',
+      add_special_tokens = True,
+      max_length = self.max_len,
+      padding = 'max_length',
+      truncation = 'longest_first'
     )
-    ids = inputs['input_ids']
-    mask = inputs['attention_mask']
+    ids = inputs('input_ids')
+    mask = inputs('attention_mask')
 
     return {
       'ids': torch.LongTensor(ids),
@@ -41,13 +37,12 @@ class CreateDataset(Dataset):
       'labels': torch.Tensor(self.y[index])
     }
 
-# BERT判定モデルの定義
 class BERTClass(torch.nn.Module):
-  def __init__(self, drop_rate, otuput_size):
+  def __init__(self, drop_rate, output_size):
     super().__init__()
     self.bert = BertModel.from_pretrained('cl-tohoku/bert-base-japanese-whole-word-masking')
     self.drop = torch.nn.Dropout(drop_rate)
-    self.fc = torch.nn.Linear(768, otuput_size)  # BERTの出力に合わせて768次元を指定 
+    self.fc = torch.nn.Linear(768, output_size)
 
   def forward(self, ids, mask):
     _, out = self.bert(ids, attention_mask=mask)
@@ -55,81 +50,62 @@ class BERTClass(torch.nn.Module):
     return out
 
 def calculate_loss_and_accuracy(model, loader, device, criterion=None):
-  """ 損失・正解率を計算"""
   model.eval()
   loss = 0.0
   total = 0
   correct = 0
   with torch.no_grad():
     for data in loader:
-      # デバイスの指定
       ids = data['ids'].to(device)
       mask = data['mask'].to(device)
       labels = data['labels'].to(device)
 
-      # 順伝播
       outputs = model(ids, mask)
 
-      # 損失計算
       if criterion != None:
         loss += criterion(outputs, labels).item()
 
-      # 正解率計算
-      pred = torch.argmax(outputs, dim=-1).cpu().numpy() # バッチサイズの長さの予測ラベル配列
-      labels = torch.argmax(labels, dim=-1).cpu().numpy()  # バッチサイズの長さの正解ラベル配列
+      pred = torch.argmax(outputs, dim=-1).cpu().numpy()
+      labels = torch.argmax(labels, dim=-1).cpu().numpy()
       total += len(labels)
       correct += (pred == labels).sum().item()
 
   return loss / len(loader), correct / total
 
 def train_model(dataset_train, dataset_valid, batch_size, model, criterion, optimizer, num_epochs, device=None):
-  """モデルの学習を実行し、損失・正解率のログを返す"""
-  # デバイスの指定
   model.to(device)
 
-  # dataloaderの作成
   dataloader_train = DataLoader(dataset_train, batch_size=batch_size, shuffle=True)
   dataloader_valid = DataLoader(dataset_valid, batch_size=len(dataset_valid), shuffle=False)
-  #dataloader_valid = DataLoader(dataset_valid, batch_size=batch_size, shuffle=True)
 
-
-  # 学習
   log_train = []
   log_valid = []
   for epoch in range(num_epochs):
-    # 開始時刻の記録
-    s_time = time.time()
+    start_time = time.time()
 
-    # 訓練モードに設定
-    model.train()
+    model.train()  # TODO: model を追う
     for data in dataloader_train:
-      # デバイスの指定
       ids = data['ids'].to(device)
       mask = data['mask'].to(device)
       labels = data['labels'].to(device)
 
-      # 勾配をゼロで初期化
       optimizer.zero_grad()
 
-      # 順伝播 + 誤差逆伝播 + 重み更新
       outputs = model(ids, mask)
       loss = criterion(outputs, labels)
       loss.backward()
       optimizer.step()
 
-    # 損失と正解率の算出
     loss_train, acc_train = calculate_loss_and_accuracy(model, dataloader_train, device, criterion=criterion)
     loss_valid, acc_valid = calculate_loss_and_accuracy(model, dataloader_valid, device, criterion=criterion)
     log_train.append([loss_train, acc_train])
     log_valid.append([loss_valid, acc_valid])
 
+    end_time = time.time()
 
-    # 終了時刻の記録
-    e_time = time.time()
-
-    # ログを出力
-    print(f'epoch: {epoch + 1}, loss_train: {loss_train:.4f}, accuracy_train: {acc_train:.4f}, loss_valid: {loss_valid:.4f}, accuracy_valid: {acc_valid:.4f}, {(e_time - s_time):.4f}sec') 
+    print(f'epoch: {epoch + 1}, loss_train: {loss_train:.4f}, accuracy_train: {acc_train:.4f}, loss_valid: {loss_valid:.4f}, accuracy_valid: {acc_valid:.4f}, {(end_time - start_time):.4f}sec') 
 
   return {'train': log_train, 'valid': log_valid}
 
-  
+
+
